8500ef98fa22466556cc027d2a5dd7ebd0d4d94b
ArtemLytvak <136497322+ArtemLytvak@users.noreply.github.com>
2023-09-26 18:03:57 +0300

    Add files via upload

---
+++ hw_mod_7/clean_folder/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='clean',
+      version='0.0.1',
+      description='Code to sort folders',
+      url='http://github.com/clean',
+      author='Artem',
+      author_email='artem@artem.com',
+      license='MIT',
+      packages=['clean'])
